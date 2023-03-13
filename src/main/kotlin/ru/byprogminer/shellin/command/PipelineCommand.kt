793f7b4dbd6dedccfbe8b1de7f19d35f3a7f5fc9
Eridan Domoratskiy <eridan200@mail.ru>
2023-03-13 03:59:43 +0300

    Implement pipeline command :)

--- src/main/kotlin/ru/byprogminer/shellin/command/PipelineCommand.kt
+++ src/main/kotlin/ru/byprogminer/shellin/command/PipelineCommand.kt
@@ -2,7 +2,11 @@ package ru.byprogminer.shellin.command
 
 import ru.byprogminer.shellin.State
 import java.io.BufferedInputStream
+import java.io.PipedInputStream
+import java.io.PipedOutputStream
 import java.io.PrintStream
+import java.util.*
+import kotlin.concurrent.thread
 
 
 /**
@@ -18,6 +22,38 @@ class PipelineCommand(
     }
 
     override fun exec(input: BufferedInputStream, output: PrintStream, error: PrintStream, state: State) {
-        TODO("Not yet implemented")
+        val inputs = List(commands.size - 1) { PipedInputStream() }
+        val outputs = (0 until commands.size - 1).map(inputs::get).map(::PipedOutputStream)
+        val exceptions = arrayOfNulls<Throwable?>(commands.size)
+
+        val threads = commands.mapIndexed { idx, cmd ->
+            return@mapIndexed thread {
+                val cmdInput = if (idx == 0) input else BufferedInputStream(inputs[idx - 1])
+                val cmdOutput = if (idx == commands.size - 1) output else PrintStream(outputs[idx])
+
+                try {
+                    cmd.exec(cmdInput, cmdOutput, error, state.copy())
+                } catch (e: Throwable) {
+                    exceptions[idx] = e
+                } finally {
+                    if (idx > 0) {
+                        cmdInput.close()
+                    }
+
+                    if (idx < commands.size - 1) {
+                        cmdOutput.close()
+                    }
+                }
+            }
+        }
+
+        threads.forEach(Thread::join)
+
+        if (exceptions.any(Objects::nonNull)) {
+            throw exceptions.filterNotNull().reduce { acc, e ->
+                acc.addSuppressed(e)
+                return@reduce acc
+            }
+        }
     }
 }
