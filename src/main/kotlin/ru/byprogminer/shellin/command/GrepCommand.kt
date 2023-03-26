d1f4b6936ccfcae070be17910443b904db8153ef
Eridan Domoratskiy <eridan200@mail.ru>
2023-03-26 19:54:00 +0300

    Implement grep

--- src/main/kotlin/ru/byprogminer/shellin/command/GrepCommand.kt
+++ src/main/kotlin/ru/byprogminer/shellin/command/GrepCommand.kt
@@ -4,18 +4,32 @@ import com.xenomachina.argparser.ArgParser
 import com.xenomachina.argparser.SystemExitException
 import com.xenomachina.argparser.default
 import ru.byprogminer.shellin.State
+import ru.byprogminer.shellin.util.readLine
 import java.io.BufferedInputStream
+import java.io.IOException
 import java.io.OutputStreamWriter
 import java.io.PrintStream
+import java.nio.file.AccessDeniedException
+import java.nio.file.Files
+import java.nio.file.NoSuchFileException
 
 
+/**
+ * Grep command filters input text, or text from file, if specified, with Java regular expression pattern.
+ *
+ * If -A specified and positive, each match following with specified amount of lines without duplicating
+ * like as in default grep.
+ */
 class GrepCommand(
-    private val args: List<String>,
+    private val argsList: List<String>,
 ) : Command {
 
+    private lateinit var args: GrepArgs
+    private lateinit var regex: Regex
+
     override fun exec(input: BufferedInputStream, output: PrintStream, error: PrintStream, state: State) {
-        val args = try {
-            ArgParser(args.subList(1, args.size).toTypedArray()).parseInto(::GrepArgs)
+        args = try {
+            ArgParser(argsList.subList(1, argsList.size).toTypedArray()).parseInto(::GrepArgs)
         } catch (e: SystemExitException) {
             val writer = OutputStreamWriter(if (e.returnCode == 0) output else error)
             e.printUserMessage(writer, "grep", 0)
@@ -23,7 +37,62 @@ class GrepCommand(
             return
         }
 
-        output.println("${args.word} ${args.caseInsensitive} ${args.linesAfter} ${args.pattern} ${args.file}")
+        initRegex()
+
+        if (args.file != null) {
+            val path = state.pwd.resolve(args.file!!)
+
+            try {
+                BufferedInputStream(Files.newInputStream(path)).use { stream ->
+                    grep(stream, output)
+                }
+            } catch (e: NoSuchFileException) {
+                error.println("File not found: \"$path\".")
+                return
+            } catch (e: AccessDeniedException) {
+                error.println("Access denied: \"$path\".")
+                return
+            } catch (e: IOException) {
+                error.println("Cannot read file: \"${e.localizedMessage}\".")
+                return
+            }
+        } else {
+            grep(input, output)
+        }
+    }
+
+    private fun initRegex() {
+        var pattern = args.pattern
+
+        if (args.word) {
+            pattern = "\\b$pattern\\b"
+        }
+
+        val options = mutableSetOf<RegexOption>()
+
+        if (args.caseInsensitive) {
+            options.add(RegexOption.IGNORE_CASE)
+        }
+
+        regex = Regex(pattern, options)
+    }
+
+    private fun grep(input: BufferedInputStream, output: PrintStream) {
+        var line = input.readLine()
+
+        var linesAfter = 0
+
+        while (line != null) {
+            if (regex.containsMatchIn(line)) {
+                output.print(line)
+                linesAfter = args.linesAfter
+            } else if (linesAfter > 0) {
+                output.print(line)
+                --linesAfter
+            }
+
+            line = input.readLine()
+        }
     }
 
     private class GrepArgs(parser: ArgParser) {
@@ -35,13 +104,25 @@ class GrepCommand(
             help = "search case-insensitive")
 
         val linesAfter by parser.storing("-A",
-            help = "specifies how much lines need to print after each match",
-            transform = String::toInt)
+            help = "specifies how much lines need to print after each match") {
+            try {
+                return@storing toInt()
+            } catch (e: IllegalArgumentException) {
+                throw SystemExitException("-A value must be an integer", 3)
+            }
+        }
             .default(0)
+            .addValidator {
+                if (value < 0) {
+                    throw SystemExitException("-A value must be non-negative", 3)
+                }
+            }
 
-        val pattern by parser.positional("PATTERN", "pattern to search for")
+        val pattern by parser.positional("PATTERN",
+            help = "pattern to search for")
 
-        val file by parser.positional("FILE", "file to search in")
+        val file by parser.positional("FILE",
+            help = "file to search in")
             .default(null)
     }
 }
